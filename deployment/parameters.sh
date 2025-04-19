da9e3b5053ce43eb0b848d5220def1c35d3be4cc
Hareeshkumar205 <85690307+Hareeshkumar205@users.noreply.github.com>
2025-04-19 12:40:25 +0530

    Create parameters.sh

---
+++ deployment/parameters.sh
@@ -0,0 +1,27 @@
+# Copyright 2022 Amazon Web Services, Inc
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+IDC_LOGIN_URL=https://d-9067cbe417.awsapps.com/start
+REGION=us-east-1
+TEAM_ACCOUNT=918362934147
+ORG_MASTER_PROFILE=org-master
+TEAM_ACCOUNT_PROFILE=aws-team
+TEAM_ADMIN_GROUP=TEAM_ADMIN
+TEAM_AUDITOR_GROUP=TEAM_AUDITOR
+TAGS="project=iam-identity-center-team environment=prod"
+CLOUDTRAIL_AUDIT_LOGS=arn:aws:cloudtrail:us-east-1:918362934147:eventdatastore/aca9a91c-9586-47eb-afdc-cbf21a1b6d6c
+SECRET_NAME=prod/aws/team
+
+# Uncomment the next line only if you have a custom domain
+# UI_DOMAIN=portal.teamtest.online
