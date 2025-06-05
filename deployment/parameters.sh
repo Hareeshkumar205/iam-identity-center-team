f4dcb498badaad0e1095f3dc2914faaf45a6bc3b
Hareeshkumar205 <85690307+Hareeshkumar205@users.noreply.github.com>
2025-06-05 11:04:28 +0530

    Update parameters.sh

--- deployment/parameters.sh
+++ deployment/parameters.sh
@@ -12,16 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-IDC_LOGIN_URL=https://d-90676dxxxx.awsapps.com/start
+IDC_LOGIN_URL=https://d-9067611efe.awsapps.com/start
 REGION=us-east-1
-TEAM_ACCOUNT=123456789101
-ORG_MASTER_PROFILE=org_master_profile
-TEAM_ACCOUNT_PROFILE=team_account_profile
-TEAM_ADMIN_GROUP="team_admin_group_name"
-TEAM_AUDITOR_GROUP="team_auditor_group_name"
-TAGS="project=iam-identity-center-team environment=prod"
-CLOUDTRAIL_AUDIT_LOGS=arn:aws:cloudtrail:us-east-1:123456789101:eventdatastore/e646f20d-7959-4682-be84-6c5b8a37cf15
-SECRET_NAME=TEAM-IDC-APP
+TEAM_ACCOUNT=331799637155
+ORG_MASTER_PROFILE=acs-org-master
+TEAM_ACCOUNT_PROFILE=acs-aws-team
+TEAM_ADMIN_GROUP=acs-cops
+TEAM_AUDITOR_GROUP=acs-cops
+CLOUDTRAIL_AUDIT_LOGS=arn:aws:cloudtrail:us-east-1:331799637155:eventdatastore/584fcc43-a652-489c-829e-6ca76915270f
+SECRET_NAME=prod/acs-aws-team
 
 # Uncomment the next line only if you have a custom domain
 # UI_DOMAIN=portal.teamtest.online
